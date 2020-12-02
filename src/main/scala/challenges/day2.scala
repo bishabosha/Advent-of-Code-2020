8470f8e103e2cae03b1d620f11d0c17a81836cf9
Jamie Thompson <bishbashboshjt@gmail.com>
2020-12-02 21:39:37 +0100

    tp alias

--- src/main/scala/challenges/day2.scala
+++ src/main/scala/challenges/day2.scala
@@ -5,20 +5,22 @@ import collectionutil.Validate._
 
 val Entry = raw"(\d+)-(\d+) ([a-z]): (\w+)".r
 
+type Policy = (Int, Int, Char, String) => Either[String, Boolean]
+
 def validateOld(lo: Int, hi: Int, char: Char, password: String) =
   required(lo <= hi) && (lo to hi).contains(password.count(_ == char))
 
 def validateNew(lo: Int, hi: Int, char: Char, password: String) =
-  required(lo <= hi & hi <= password.length)
+  required(lo <= hi && hi <= password.length)
   && (password(lo - 1) == char ^ password(hi - 1) == char)
 
-def parseEntry(policy: (Int, Int, Char, String) => Either[String, Boolean])(entry: String) = entry match
+def parseEntry(policy: Policy)(entry: String) = entry match
   case Entry(l, u, c, p) =>
     policy(l.toInt, u.toInt, c(0), p)
   case line =>
     Left(s"$line is not a valid entry, expected $Entry")
 
-def _n(policy: (Int, Int, Char, String) => Either[String, Boolean]) =
+def _n(policy: Policy) =
   for
     lines   <- io.unsafe.lines(challenge(day=2, part=0))
     matches <- collectionutil.traverse(lines.toList)(parseEntry(policy))
